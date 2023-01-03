f984e2d7a84fba9ecb0e009ec7c40d329b058053
Domus Aurea <philip.doge.kaminski@gmail.com>
2023-01-03 17:31:43 +0000

    create table script runs

--- db/scripts/createTimestampsTable.js
+++ db/scripts/createTimestampsTable.js
@@ -1,11 +1,11 @@
-import { query } from "../../index.js";
+import { query } from "../index.js";
 
 const sqlString = `CREATE TABLE IF NOT EXISTS timestamps (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, day_name TEXT, day INT, month TEXT, year INT, date TEXT, day_number INT );`;
 
 async function createEmailsTable() {
   const res = await query(sqlString);
   console.log(res);
-  console.log("created emails table");
+  console.log("created timestamps table");
 }
 
 createEmailsTable();
